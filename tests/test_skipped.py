7ab1d7c81eada1cc19e1bb0a9453a18ce740f2df
Alexandr Rusakov <alexanderrusakov01@mail.ru>
2026-06-04 22:38:15 +0400

    Lesson_11

--- tests/test_skipped.py
+++ tests/test_skipped.py
@@ -12,9 +12,3 @@ def test_skipped2():
 @pytest.mark.skip
 def test_skipped3():
     pass
-
-
-def test_skipped11():
-    # какая‑то тестовая логика
-    result = some_function()
-    assert result is not None  # обязательный assert
